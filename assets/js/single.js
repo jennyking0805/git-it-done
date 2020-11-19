ef84609ccd0e1b66f37a2730d8de18685fee9fe6
Jenny King <jennyking0805@me.com>
2020-11-19 13:05:01 -0500

    handled errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,20 @@ var issueContainerEl = document.querySelector("#issues-container");
 
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+};
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -11,11 +25,11 @@ var getRepoIssues = function(repo) {
                 displayIssues(data);
 
             if (response.headers.get("Link")) {
-                displayIssues(repo);
+                displayWarning(repo);
             }
             });
         } else {
-            alert("There was a probelm with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -63,4 +77,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
